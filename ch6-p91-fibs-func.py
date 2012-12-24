1e3b51510a6b225b5e2058298cb811287cb38ec2
Fox.Lee.office <jiaqing.li@outlook.com>
2012-12-24 16:53:36 +0800

    updated fibs func

--- ch6-p91-fibs-func.py
+++ ch6-p91-fibs-func.py
@@ -4,6 +4,6 @@ def fibs(num): #计算多少个斐波那契数列
     for i in xrange(num-2):
         result.append(result[-2]+result[-1]) #永远都是倒数第二加上倒数第一
     return result
-
+ 
 num = raw_input('num:?')
-print fibs(int(num))
+print fibs(int(num)) 
